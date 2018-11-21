17674b5153fa665b9b48927d7b13c9c668f3702d
Amrita Venkatraman <amritav.ca@gmail.com>
2018-11-21 13:07:12 -0800

    finished response schedule

---
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ScheduleResponseActivity.java
@@ -0,0 +1,13 @@
+package android2018.amrita.com.homemadescreens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleResponseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_response);
+    }
+}
