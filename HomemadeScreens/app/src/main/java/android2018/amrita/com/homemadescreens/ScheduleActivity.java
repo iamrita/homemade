9eea5c7eb5fd84701eb9820313874b9010199a84
Amrita Venkatraman <amritav.ca@gmail.com>
2018-11-20 23:30:09 -0800

    renamed and modified schedule

--- HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ScheduleActivity.java
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ScheduleActivity.java
@@ -6,6 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.*;
+
+
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -13,8 +18,16 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    /*
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -23,7 +36,7 @@ public class ScheduleActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
