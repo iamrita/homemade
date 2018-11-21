8e880a59a6d4e5bb5ed3ce375d65ea28104a9f34
Paola martinez <paomart@stanford.edu>
2018-11-21 01:33:45 -0600

    added profile screen

--- HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/profileActivity.java
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/profileActivity.java
@@ -7,13 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ScheduleActivity extends AppCompatActivity {
+public class profileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
-        
+        setContentView(R.layout.activity_profile);
+
     }
 
 }
