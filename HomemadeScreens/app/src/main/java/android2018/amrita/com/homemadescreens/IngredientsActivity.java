56b0575bb631f1e440ed179023e07d9b37a88e25
erincohenja <erinc95@stanford.edu>
2018-11-21 10:33:00 -0500

    2 new screens/activities
    confirmation and ingredients

---
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/IngredientsActivity.java
@@ -0,0 +1,13 @@
+package android2018.amrita.com.homemadescreens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IngredientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredients);
+    }
+}
