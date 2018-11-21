833060825333bcbe695ee2e5c5e21cd2ba7b34a0
Paola martinez <paomart@stanford.edu>
2018-11-21 00:26:53 -0600

    pao's1stchange
    lets see if it works.

--- HomemadeScreens/app/src/androidTest/java/android2018/amrita/com/homemadescreens/ExampleInstrumentedTest.java
+++ HomemadeScreens/app/src/androidTest/java/android2018/amrita/com/homemadescreens/ExampleInstrumentedTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * this is my comment
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
