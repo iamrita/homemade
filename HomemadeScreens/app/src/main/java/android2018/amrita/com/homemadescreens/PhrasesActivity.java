bd804ab2801e41d192296671d683df978367e262
Amrita Venkatraman <amritav.ca@gmail.com>
2018-11-24 23:02:15 -0800

    live translator/phrases linked but are separate from flow of app

--- HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/PhrasesActivity.java
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/PhrasesActivity.java
@@ -1,7 +1,11 @@
 package android2018.amrita.com.homemadescreens;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        ImageButton back = (ImageButton)findViewById(R.id.back_phrases);
+        final Context context = this;
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, LiveMealActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
