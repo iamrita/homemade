e74628593868cfe139315a3508de776236837029
Amrita Venkatraman <amritav.ca@gmail.com>
2018-11-24 21:58:42 -0800

    flow working except for confirmation

--- HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ConfirmActivity.java
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ConfirmActivity.java
@@ -3,11 +3,12 @@ package android2018.amrita.com.homemadescreens;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ConfirmationActivity extends AppCompatActivity {
+public class ConfirmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirmation);
+        setContentView(R.layout.activity_confirm);
+        System.out.println("OHHHH YEAHHH");
     }
 }
--- HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ExploreV2Activity.java
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/ExploreV2Activity.java
@@ -1,7 +1,11 @@
 package android2018.amrita.com.homemadescreens;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableRow;
 
 public class ExploreV2Activity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class ExploreV2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_v2);
+        TableRow row = (TableRow)findViewById(R.id.table_clicked);
+        final Context context = this;
+
+        row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, profileActivityLeila.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
