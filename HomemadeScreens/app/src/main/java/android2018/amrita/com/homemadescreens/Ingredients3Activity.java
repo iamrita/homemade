27b5cf2783c1e99f59d7cb5c69f7f7b842931b0a
Paola martinez <paomart@stanford.edu>
2018-12-01 15:54:44 -0800

    all ingredients added

--- HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/Ingredients3Activity.java
+++ HomemadeScreens/app/src/main/java/android2018/amrita/com/homemadescreens/Ingredients3Activity.java
@@ -1,17 +1,13 @@
 package android2018.amrita.com.homemadescreens;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-public class UpcomingActivity extends AppCompatActivity {
+public class Ingredients3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upcoming);
+        setContentView(R.layout.activity_ingredients3);
     }
 }
